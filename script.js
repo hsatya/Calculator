8302f0ec132af983b49a8b21178053703000b1e0
Harshit Satya <harshitsatya5@gmail.com>
2022-01-07 17:52:43 +0530

    started working on JavaScript part

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+'use strict';
 
+const calculatorDisplay = document.querySelector('h1');
+const inputBtns = document.querySelectorAll('button');
+const clearBtn = document.getElementById('clear-btn');
+
+function sendNumberValue(number) {
+  // if current display value is 0, replace it, if not conact number
+  const displayValue = calculatorDisplay.textContent;
+  calculatorDisplay.textContent =
+    displayValue === '0' ? number : displayValue + number;
+}
+
+// Add Event Listners for numbers, operators and decimal buttons
+inputBtns.forEach(inputBtn => {
+  if (inputBtn.classList.length === 0) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('operator')) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  } else if (inputBtn.classList.contains('decimal')) {
+    inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+  }
+});
